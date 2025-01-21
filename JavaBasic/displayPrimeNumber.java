3425d944e5b1765ea73247a268c4acc965876c34
AniketKumar15 <110549582+AniketKumar15@users.noreply.github.com>
2025-01-21 20:50:13 +0530

    Add More Code

---
+++ JavaBasic/displayPrimeNumber.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class displayPrimeNumber {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    int flag = 0;
+
+    for (int i = 1; i <= n; i++) {
+      if (i == 0 || i == 1) {
+        continue;
+      }
+      flag = 1;
+      for (int j = 2; j < i; j++) {
+        if (i % j == 0) {
+          flag = 0;
+        }
+
+      }
+      if (flag == 1) {
+        System.out.println(i);
+      }
+    }
+    // System.out.println(flag);
+    sc.close();
+  }
+}
